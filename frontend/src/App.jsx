45ce4586d07ea0357c218f5fc43384195fcf8f00
somesh520 <someshtiwari.in@gmail.com>
2026-08-31 22:25:14 +0530

    remove websocket forcing to allow fallback to polling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,6 @@ import { io } from 'socket.io-client';
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const socket = io(API_URL, {
-  transports: ['websocket'], // Force WebSocket only to avoid polling errors
   reconnectionRequests: 10,
   autoConnect: false,
   withCredentials: true,
